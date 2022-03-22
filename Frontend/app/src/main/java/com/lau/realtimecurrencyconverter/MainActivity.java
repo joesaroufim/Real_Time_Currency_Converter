4f023f52f912c2d67708a532e27a79dad3a53720
Rami <ramiakl33@gmail.com>
2022-03-22 18:52:43 +0200

    Creating a function that convert to LBP

--- Frontend/app/src/main/java/com/lau/realtimecurrencyconverter/MainActivity.java
+++ Frontend/app/src/main/java/com/lau/realtimecurrencyconverter/MainActivity.java
@@ -3,19 +3,42 @@ package com.lau.realtimecurrencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText dollar_amount, lbp_amount;
-    double dollar, lbp, rate;
+    EditText input;
+    TextView result;
+    double  amount,rate, result_amount;
+    String amount_str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        input = (EditText) findViewById(R.id.input);
+        result = (TextView) findViewById(R.id.result);
+        rate = 22000;
     }
 
+    public void toLbp(View view){
+
+        String amount_str  = input.getText().toString();
+
+        if(amount_str.isEmpty()){
+            Toast.makeText(getApplicationContext(), "Please enter a value", Toast.LENGTH_LONG).show();
+        }
+        else{
+            amount = Double.parseDouble(amount_str);
+            result_amount = amount*rate;
+
+            result.setText(""+result_amount);
+        }
+    }
 
 
 }
