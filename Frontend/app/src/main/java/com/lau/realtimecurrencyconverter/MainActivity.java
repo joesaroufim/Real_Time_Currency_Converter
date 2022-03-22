46415b5ac9108d410de48d17ed91173ae4065bd9
Rami <ramiakl33@gmail.com>
2022-03-22 18:35:05 +0200

    Initializing the variables

--- Frontend/app/src/main/java/com/lau/realtimecurrencyconverter/MainActivity.java
+++ Frontend/app/src/main/java/com/lau/realtimecurrencyconverter/MainActivity.java
@@ -3,12 +3,19 @@ package com.lau.realtimecurrencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText dollar_amount, lbp_amount;
+    double dollar, lbp, rate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
 }
