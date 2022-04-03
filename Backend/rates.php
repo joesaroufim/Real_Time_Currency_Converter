dc69c7c2494be312dc4c5317176af1c2e1d60f61
Rami <ramiakl33@gmail.com>
2022-04-03 13:01:12 +0300

    Creating a post api and getting the variables send from android studio

--- Backend/rates.php
+++ Backend/rates.php
@@ -3,18 +3,10 @@
 include ("db_info.php");
 
 
-$x = $_POST["data"];
-$y = "hello";
-$z = "helloo";
+$rate = $_POST["rate"];
+$conversion_type = $_POST["conversion"];
+$lbp = $_POST["lbp"];
+$usd = $_POST["usd"];
 
-
-$response = $x.$y;
-
-$json_response = json_encode($response);
-echo($json_response);
-
-$query = $mysqli->prepare("INSERT INTO rates (date, lbp, usd) VALUES (?, ?, ?)");
-$query->bind_param("sss", $y, $z, $x);
-$query->execute();
 
 ?>
