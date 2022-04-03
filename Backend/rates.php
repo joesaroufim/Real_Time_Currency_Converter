469aaf8f611e390754372578fd0f4ec705b9d076
Rami <ramiakl33@gmail.com>
2022-04-03 13:04:45 +0300

    Writing the query and executing it to the xampp

--- Backend/rates.php
+++ Backend/rates.php
@@ -11,4 +11,8 @@ $usd = $_POST["usd"];
 $json_response = json_encode($response);
 echo($json_response);
 
+$query = $mysqli->prepare("INSERT INTO rates (rate, lbp, usd, conversion_type) VALUES (?, ?, ?, ?)");
+$query->bind_param("ssss", $rate, $lbp, $usd, $conversion_type);
+$query->execute();
+
 ?>
