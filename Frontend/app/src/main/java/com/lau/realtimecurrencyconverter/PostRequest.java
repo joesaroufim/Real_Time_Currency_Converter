79e6c3c5615df22df9463a82df67a49e844a9d3b
Joe Saroufim <joesaroufim.os@gmail.com>
2022-04-03 21:30:18 +0300

    Commenting the PostRequest class

--- Frontend/app/src/main/java/com/lau/realtimecurrencyconverter/PostRequest.java
+++ Frontend/app/src/main/java/com/lau/realtimecurrencyconverter/PostRequest.java
@@ -16,28 +16,34 @@ public class PostRequest extends AsyncTask<String, Void, String> {
 
     @Override
     protected String doInBackground(String... params) {
+        //The method take String parameters and send data to the received url.
+
+        //Storing data in String objects
         String rate = params[0];
         String conversion_type = params[1];
         String lbp = params[2];
         String usd = params[3];
         String str_url = params[4];
+
         try {
+            // Creating a new URL connection with PHP.
             URL url = new URL(str_url);
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.setRequestMethod("POST");
             urlConnection.setDoInput(true);
             urlConnection.setDoOutput(true);
 
-            OutputStream out = urlConnection.getOutputStream();
+            OutputStream out = urlConnection.getOutputStream(); //Initializing OutputStream Object.
 
-            BufferedWriter br = new BufferedWriter(new OutputStreamWriter(out, "UTF-8"));
+            BufferedWriter br = new BufferedWriter(new OutputStreamWriter(out, "UTF-8")); //Initializing BufferedWriter Object
 
+            // Setting the variables to be sent to the URL
             String post_data = URLEncoder.encode("rate", "UTF-8")+"="+URLEncoder.encode(rate, "UTF-8")+"&"
                     +URLEncoder.encode("conversion", "UTF-8")+"="+URLEncoder.encode(conversion_type, "UTF-8")+"&"
                     +URLEncoder.encode("lbp", "UTF-8")+"="+URLEncoder.encode(lbp, "UTF-8")+"&"
                     +URLEncoder.encode("usd", "UTF-8")+"="+URLEncoder.encode(usd, "UTF-8");
 
-            br.write(post_data);
+            br.write(post_data); //Writing and sending data.
             br.flush();
             br.close();
             out.close();
@@ -46,6 +52,7 @@ public class PostRequest extends AsyncTask<String, Void, String> {
 
             urlConnection.disconnect();
 
+            //Catching exceptions
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
