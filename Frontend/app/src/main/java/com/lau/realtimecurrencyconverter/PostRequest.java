179f4a56b6b9f569007a96a3f53573e35f2bb5e2
Joe Saroufim <joesaroufim.os@gmail.com>
2022-04-03 10:50:16 +0300

    Created PostRequest class

---
+++ Frontend/app/src/main/java/com/lau/realtimecurrencyconverter/PostRequest.java
@@ -0,0 +1,13 @@
+package com.lau.realtimecurrencyconverter;
+
+import android.os.AsyncTask;
+
+public class PostRequest extends AsyncTask<String, Void, String> {
+
+    String result;
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+}
