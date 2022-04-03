6c7ef10114d5ba3e020bd8322e6e287a8385905d
Rami <ramiakl33@gmail.com>
2022-04-03 13:52:49 +0300

    Adding a date in php and in the database

--- Backend/rates.php
+++ Backend/rates.php
@@ -8,11 +8,13 @@ $conversion_type = $_POST["conversion"];
 $lbp = $_POST["lbp"];
 $usd = $_POST["usd"];
 
+$date = date('d-m-y h:i:s'); 
+
 $json_response = json_encode($response);
 echo($json_response);
 
-$query = $mysqli->prepare("INSERT INTO rates (rate, lbp, usd, conversion_type) VALUES (?, ?, ?, ?)");
-$query->bind_param("ssss", $rate, $lbp, $usd, $conversion_type);
+$query = $mysqli->prepare("INSERT INTO rates (rate, lbp, usd, conversion_type,date) VALUES (?, ?, ?, ?,?)");
+$query->bind_param("sssss", $rate, $lbp, $usd, $conversion_type,$date);
 $query->execute();
 
 ?>
